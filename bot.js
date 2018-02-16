39bc0e8c239f4cbece5e46854517b4e53845bcb6
MCninja <ninja.jpb@gmail.com>
2018-02-16 06:46:25 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,10 +33,10 @@ bot.on("guildCreate", (guild) => {
 bot.on('message', (message) => {
 if(message.author.bot) return;
     if (message.author.id !='270198738570444801' && message.author.id !='394019914157129728' && message.author.id !='204255221017214977') {
-  if (message.guild.id !='110373943822540800' && message.guild.id !='149220234690166785' &&  message.guild.id !='343024903735214081' &&  message.guild.id !='380174860523143169' && message.guild.id !='398122224638492676' && message.guild.id !='264445053596991498') {
+  if (message.guild.id !='110373943822540800' && message.guild.id !='414039704514592770' && message.guild.id !='149220234690166785' &&  message.guild.id !='343024903735214081' &&  message.guild.id !='380174860523143169' && message.guild.id !='398122224638492676' && message.guild.id !='264445053596991498') {
 if(message.channel.id == "413185119080153088") return;    
 if(message.channel.id == "413825688076943362") return;
-	if (message.content.match(words)) {
+	if (message.content.match(/(b i t c|bit c|b itc|b it c|kys|k y s|k ys|ky s|dick| dic |d l c|dlc|d i c| dic|cunt|c u n t|bitch|bish|shit|fuc|p0rn|nigg|d1c|d 1 c|n l g|n i g|n 1 g|n1g|nlg|n!g|bast|wank|f ag|fa g|fag|f4g|f 4 g|f a g|f @ g|f@g|sex |tits|8--|8==|dild|porn|fuk|slut|whore|retard|f u c k|cock|nibba|f u k|f.u.c| ass)/gi)) {
    message.reply("You're not allowed to say that...")
    message.delete()
     console.log(`Deleted message from ${message.author} ${message.author.username}: ${message.content}`)
@@ -97,6 +97,7 @@ if (message.content == 'sex') {
 	   if(newMessage.guild.id == "264445053596991498 ") return;
 	   if(oldMessage.guild.id == "110373943822540800") return;
 	   if(newMessage.guild.id == "264445053596991498") return;
+	   if(oldMessage.guild.id == "414039704514592770") return;
     if (oldMessage.content.match(/(b i t c|bit c|b itc|b it c|kys|k y s|k ys|ky s|dick| dic |d l c|dlc|d i c| dic|cunt|c u n t|bitch|bish|shit|fuc|p0rn|nig|d1c|d 1 c|n l g|n i g|n 1 g|n1g|nlg|n!g|bast|wank|f ag|fa g|fag|f4g|f 4 g|f a g|f @ g|f@g|sex |tits|8--|8==|dild|porn|fuk|slut|whore|retard|f u c k|cock|nibba|f u k|f.u.c)/gi)) {
 
  
@@ -445,7 +446,7 @@ message.delete()
 		
 if (message.content == '+test')  {
     const logchannel = bot.channels.get("399688995283533824")
-    logchannel.send("lol");
+    console.log(words.words)
 		}
 		});
  //End of Testing Commands
