da6bde516d1b3794817d922cf86829386d7673a0
MCninja <ninja.jpb@gmail.com>
2018-02-16 04:43:37 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 const logchannel = bot.channels.get("399688995283533824")
 const serverlistchannel = bot.channels.get("413831069117186078")
+const words = require("./words.json")
 //Start of Status + Game Playing
 
 bot.on("ready", () => {
@@ -35,7 +36,7 @@ if(message.author.bot) return;
   if (message.guild.id !='110373943822540800' && message.guild.id !='149220234690166785' &&  message.guild.id !='343024903735214081' &&  message.guild.id !='380174860523143169' && message.guild.id !='398122224638492676' && message.guild.id !='264445053596991498') {
 if(message.channel.id == "413185119080153088") return;    
 if(message.channel.id == "413825688076943362") return;
-	if (message.content.match(/(b i t c|bit c|b itc|b it c|kys|k y s|k ys|ky s|dick| dic |d l c|dlc|d i c| dic|cunt|c u n t|bitch|bish|shit|fuc|p0rn|nigg|d1c|d 1 c|n l g|n i g|n 1 g|n1g|nlg|n!g|bast|wank|f ag|fa g|fag|f4g|f 4 g|f a g|f @ g|f@g|sex |tits|8--|8==|dild|porn|fuk|slut|whore|retard|f u c k|cock|nibba|f u k|f.u.c)/gi)) {
+	if (message.content.match(words)) {
    message.reply("You're not allowed to say that...")
    message.delete()
     console.log(`Deleted message from ${message.author} ${message.author.username}: ${message.content}`)
@@ -95,7 +96,7 @@ if (message.content == 'sex') {
 	   if(oldMessage.guild.id == "110373943822540800") return;
 	   if(newMessage.guild.id == "264445053596991498 ") return;
 	   if(oldMessage.guild.id == "110373943822540800") return;
-
+	   if(newMessage.guild.id == "264445053596991498") return;
     if (oldMessage.content.match(/(b i t c|bit c|b itc|b it c|kys|k y s|k ys|ky s|dick| dic |d l c|dlc|d i c| dic|cunt|c u n t|bitch|bish|shit|fuc|p0rn|nig|d1c|d 1 c|n l g|n i g|n 1 g|n1g|nlg|n!g|bast|wank|f ag|fa g|fag|f4g|f 4 g|f a g|f @ g|f@g|sex |tits|8--|8==|dild|porn|fuk|slut|whore|retard|f u c k|cock|nibba|f u k|f.u.c)/gi)) {
 
  
@@ -450,4 +451,4 @@ if (message.content == '+test')  {
  //End of Testing Commands
 		
 
-bot.login('token');
+bot.login('Mzk0MDE5OTE0MTU3MTI5NzI4.DSREAQ.ZzIsakoZgMSlV4AKUU_vptyqLOc');
