883224fedc9c2be3865e5ebc58202361bd0e2a3c
MCninja <ninja.jpb@gmail.com>
2018-03-11 18:41:41 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,7 +33,9 @@ if(message.guild.id == "264445053596991498") return
 //End Of Bot Join Message
 //Start of Basic Filter
 bot.on('message', async (message) => {
-if(!message.guild) return;
+
+if(!message.guild) return;	
+if(message.channel.nsfw) return;
 if(message.author.bot) return;
     if (message.author.id !='270198738570444801' && message.author.id !='394019914157129728' && message.author.id !='204255221017214977') {
   if (message.guild.id !='110373943822540800' && message.guild.id !='414039704514592770' && message.guild.id !='149220234690166785' &&  message.guild.id !='343024903735214081' &&  message.guild.id !='380174860523143169' && message.guild.id !='264445053596991498') {
@@ -41,9 +43,9 @@ if(message.channel.id == "413185119080153088") return;
 if(message.channel.id == "413825688076943362") return;
 if (message.content.match(/(b i t c|bit c|b itc|b it c|kys|k y s|k ys|ky s|dick| dic |d l c|dlc|d i c| dic|cunt|c u n t|bitch|bish|shit|fuc|p0rn|nigg|d1c|d 1 c|n l g|n 1 g|n1g|nlg|n!g|bast|wank|f ag|fa g|fag|f4g|f 4 g|f a g |f @ g|f@g|sex |tits|8--|8==|dild|porn|fuk|slut|whore|retard|f u c k|cock|nibba|f u k|f.u.c| ass)/gi)) {
    message.delete()
-   nomsg = await message.reply("You're not allowed to say that...")
+   const popnomsg = await message.reply("You're not allowed to say that...")
     setTimeout(function() {
-nomsg.delete()
+popnomsg.delete()
 }, 3000);
 	
     console.log(`Deleted message from ${message.author} ${message.author.username}: ${message.content}`)
@@ -212,6 +214,7 @@ nomsg.delete()
 
    bot.on('messageUpdate', async (newMessage, oldMessage, guild) => {
 if(!oldMessage.guild) return;
+if(oldMessage.channel.nsfw) return;
 
        if(oldMessage.guild.id == "149220234690166785") return;
 	   if(oldMessage.guild.id == "110373943822540800") return;
@@ -609,37 +612,50 @@ if (message.content == '+heckyouroomnerd') {
 //End of Fun Reactions
 
 //Start of Use Commands
-bot.on('message', message => {
+bot.on('message', async (message) => {
         const logchannel = bot.channels.get("399688995283533824")
 
 if (message.content == '+help') {
 	message.delete()
-    message.reply('Hello and thanks for using JacobSux, just for your information, this is non-customizable, atleast right now...\n``+help`` : Displays this list\n``+support`` : Sends invite to support server to DMs\n``+inv`` : Sends link to invite\n``+invite`` : Same as +inv\n``+github`` : Displays the github link\n``+update`` : Displays the most recent update to the bot\n``+donate`` : Donate towards the development of JacobSux')
-    console.log(`${message.author} ${message.author.username} Requested Help...`)
+ const helpmsg = await message.reply('Hello and thanks for using JacobSux, just for your information, this is non-customizable, atleast right now...\n(This Message will Self Destruct In 30 Seconds)\n``+help`` : Displays this list\n``+support`` : Sends invite to support server to DMs\n``+inv`` : Sends link to invite\n``+invite`` : Same as +inv\n``+github`` : Displays the github link\n``+update`` : Displays the most recent update to the bot\n``+donate`` : Donate towards the development of JacobSux')
+     setTimeout(function() {
+		helpmsg.edit(":boom:")
+		 setTimeout(function() {
+		 helpmsg.delete()
+	 }, 5000);	 
+	 },25000 );
+	console.log(`${message.author} ${message.author.username} Requested Help...`)
     logchannel.send(`${message.author} ${message.author.username} Requested Help...`)
 }
 if (message.content == '+support') {
-	message.delete()
-    message.reply('Support Server Sent to DMs')
+	message.delete()           
+   const msg = await message.reply("Support Server Sent to DM's")
+    setTimeout(function() {
+msg.delete()
+}, 6000);
     message.author.send('https://discord.gg/mx6Gcdb')
     console.log(`${message.author} ${message.author.username} Requested Support...`)
     logchannel.send(`${message.author} ${message.author.username} Requested Support...`)
 } 
 if (message.content == '+inv') {
 	message.delete()
-    message.reply('`Invite me:` http://jt3ch.cf/jacobsux')
+    message.reply('`Invite me:` https://www.jt3ch.net/jacobsux')
     console.log(`${message.author} ${message.author.username} Requested an Invite...`)
     logchannel.send(`${message.author} ${message.author.username} Requested an Invite...`)
 } 
 if (message.content == '+invite') {
 	message.delete()
-    message.reply('`Invite me:` http://jt3ch.cf/jacobsux')
+    message.reply('`Invite me:` https://www.jt3ch.net/jacobsux')
     console.log(`${message.author} ${message.author.username} Requested an Invite...`)
     logchannel.send(`${message.author} ${message.author.username} Requested an Invite...`)
 } 
 if (message.content == '+github') {
-	message.delete()
-    message.reply('https://github.com/mcriper/jacobsux')
+		message.delete()           
+   const msg = await message.reply("Github REPO Sent to DM's")
+    setTimeout(function() {
+msg.delete()
+}, 6000);
+    message.author.send('https://github.com/mcriper/jacobsux')
     console.log(`${message.author} ${message.author.username} Requested GitHub...`)
     logchannel.send(`${message.author} ${message.author.username} Requested GitHub...`)
 } 
@@ -651,8 +667,14 @@ if (message.content == '+donate') {
 } 
 if (message.content == '+update') {
 	message.delete()
-    message.reply('Most recent update: \n 2/26/18: Added the +github command\n 2/24/18: All use commands (which can be listed with +help) Now delete request! Basically, If you type +help, youll get the commands, AND it will delete the "+help" command message...\n 2/24/18: Added the +updates command :> \n(Too get updates on recent updates, join the discord! (+support) Have fun!)')
-    console.log(`${message.author} ${message.author.username} Requested Update...`)
+  const updatemsg = await message.reply('Most recent updates: \n(This message will self destruct in 10seconds\n3/3/18: All channels set to NSFW (the little switch in the channel settings) will be ignored from cursing... \n2/26/18: Update command now self destructs after 10 seconds :>\n2/26/18: Help command now self destructs after 30s\n2/26/18: +support and +github now send links to DMs instead of in public chat\n2/26/18: Added the +github command\n2/24/18: All use commands (which can be listed with +help) Now delete request! Basically, If you type +help, youll get the commands, AND it will delete the "+help" command message...\n2/24/18: Added the +updates command :> \n(Too get updates on recent updates, join the discord! (+support) Have fun!)')
+    setTimeout(function() {
+		updatemsg.edit(":boom:")
+		setTimeout(function() {
+			updatemsg.delete()
+		}, 1000)
+	}, 9000);
+	console.log(`${message.author} ${message.author.username} Requested Update...`)
     logchannel.send(`${message.author} ${message.author.username} Requested Update...`)
 } 
 
