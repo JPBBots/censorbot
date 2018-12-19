2d8c090b629b815acc1b8f213f885005cddae414
JPBBerry <ninja.jpb@gmail.com>
2018-12-18 20:59:07 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,23 @@
+const config = {
+    "ownerID": owners id,
+    "admins": [list, of, admins],
+    "support": [list,of,support,helpers],
+    "token": BOT TOKEN,
+    "dbltoken": DISCORDBOTLIST APIKEY,
+    "plotlyapi": PLOTLY APIKEY,
+    "prefix": PREFIX,
+    "name": BOT NAME,
+    "website": WEBSITE ROOT,
+    "invitesite": LINK TO INVITE,
+    "donate": DONATION LINK,
+    "github": GITHUB LINK,
+    "support": SUPPORT SERVER INVITE,
+    "commands": {
+        "help": "Displays this list", EXAMPLE
+        COMMANDNAME: COMMANDDESCRIPTION,
+        JSON.OBJ
+        }
+  };
+  
+  module.exports = config;
+  
