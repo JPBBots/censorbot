0a488fd63a84389e8c96d199ab915822e5a9c9df
jpbberry <jb@jt3ch.net>
2020-03-20 17:32:55 -0700

    Made test stringify regex statements for easier debug

--- src/commands/test.js
+++ src/commands/test.js
@@ -2,7 +2,7 @@ exports.run = async function (message, args) {
   const db = await this.client.db.config(message.guild_id)
 
   const res = this.client.filter.test(args.join(' '), db.base, db.languages, db.filter, db.uncensor)
-
+  res.arg.forEach((arg,i)=>{ res.arg[i] = arg.toString() })
   this.send(
     this.embed
       .description('```json\n' +
