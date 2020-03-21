8d1796b0088add7d2c12acf49b213b7cbf06d297
jpbberry <jb@jt3ch.net>
2020-03-21 15:59:27 -0700

    DM owner on join readded

--- src/events/GUILD_CREATE.js
+++ src/events/GUILD_CREATE.js
@@ -15,4 +15,13 @@ module.exports = function (guild) {
   this.log(6, 18, guild.id, guild.name)
 
   this.db.config(guild.id)
+
+  if (available) return
+
+  this.interface.dm(guild.owner_id,
+    this.embed
+      .title(`Thanks for inviting me to your server! (${guild.name})`)
+      .description(`We hope you enjoy your time, and hope we can make your server a cleaner place!\n\nHere are some useful resources:\nDashboard: https://censorbot.jt3ch.net/dash\n[Support Server](${this.config.support})\n[Premium](https://censorbot.jt3ch.net/premium)\n\nThanks again!`)
+      .footer(guild.id)
+  )
 }
