cb493d6f466e7061181b0a74011108e437f8a7ac
jpbberry <jb@jt3ch.net>
2020-03-20 17:28:20 -0700

    Use userGuilds in admin endpoint

--- src/dashboard/routes/api/users.js
+++ src/dashboard/routes/api/users.js
@@ -2,8 +2,10 @@ module.exports = function (r) {
   r.use(this.adminMiddle(true))
 
   r.get('/:userid/guilds', (req, res) => {
+    const guilds = this.client.userGuilds.get(req.params.userid) || []
+
     res.json(
-      this.client.guilds.filter(x => x.owner_id === req.params.userid).map(x => {
+      this.client.guilds.filter(x => guilds.includes(x.id)).map(x => {
         return {
           id: x.id,
           name: x.name
