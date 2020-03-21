28672f29353342ea58bfa34e1cfa4aaae18e4cb6
jpbberry <jb@jt3ch.net>
2020-03-21 06:20:31 -0700

    FIxed updates board

--- src/dashboard/routes/index.js
+++ src/dashboard/routes/index.js
@@ -1,4 +1,5 @@
 const encodeJSON = require('../../../util/encodeJSON')
+const Express = require('express')
 
 module.exports = function (r) {
   r.get('/invite', (req, res) => {
@@ -11,4 +12,32 @@ module.exports = function (r) {
       })
     )
   })
+
+  r.get('/updates/:v', (req, res, next) => {
+    var url = 'https://censorbot.jt3ch.net/updates'
+    var z = require('/home/jpb/websites/censorbot/updates/updates.js')
+    var the = z.find((x) => x.v == req.params.v)
+    if (!the) return next()
+    res.send(`
+    <!DOCTYPE html>
+    <html>
+      <head>
+          <meta property="og:title" content="Update v${the.v}">
+          <meta property="og:url" content="https://censorbot.jt3ch.net/updates/${req.params.v}">
+          <meta property="og:description" content="${the.desc}">
+      </head>
+      <body>
+          <h1>${req.params.v}</h1>
+      </body>
+      <script>
+          window.location.replace("${url}#${req.params.v}");
+      </script>
+    </html>`)
+  })
+
+  r.get('/updates', (req, res) => {
+    res.sendFile('/home/jpb/websites/censorbot/updates/index.html')
+  })
+
+  r.use('/updates', Express.static('/home/jpb/websites/censorbot/updates'))
 }
