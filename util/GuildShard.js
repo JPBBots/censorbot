206171945ff781c187def025ea6e8554727c40c1
jpbberry <jb@jt3ch.net>
2020-03-26 17:02:28 -0700

    Fixed GuildShard

--- util/GuildShard.js
+++ util/GuildShard.js
@@ -5,7 +5,7 @@ module.exports = (guild, amount) => {
   const guildID = String(guild)
   let shard
   for (let i = 0; i < amount; i++) {
-    if ((BigInt(guildID) >> BigInt(22)) % BigInt(amount) === i) { shard = i; break }
+    if ((BigInt(guildID) >> BigInt(22)) % BigInt(amount) == i) { shard = i; break } // eslint-disable-line eqeqeq
   }
   return shard
 }
