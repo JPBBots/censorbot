5a537236c43602015306d2cf8930a54bf74eef99
jpbberry <jb@jt3ch.net>
2020-03-26 17:22:35 -0700

    Outfit restart command with destroying

--- src/commands/restart.js
+++ src/commands/restart.js
@@ -2,10 +2,19 @@ exports.run = function (message, args) {
   this.delete()
   if (!args[0]) return process.exit()
 
+  if (message.content.includes('-d')) return this.client.killShard(args[1])
+    .then(x => {
+      this.send('Shard Destroyed and Restarted')
+    })
+    .catch(err => {
+      this.send(`\`Error\`\n${err.message}`)
+    })
+
   const shard = this.client.shards.get(parseInt(args[0]))
   if (!shard) return this.send('Invalid Shard')
 
   shard.restart()
+  this.send('Shard Killed and Resumed')
 }
 
 exports.info = {
