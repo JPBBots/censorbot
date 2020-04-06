b29fc4f490e3e571f3752e476f9d726bb1b0c4a0
jpbberry <jb@jt3ch.net>
2020-04-06 13:10:32 -0700

    Added jsdoc for request

--- req/index.js
+++ req/index.js
@@ -1,1 +1,19 @@
 module.exports = require('./src/API.js')
+
+/**
+ * Request library made out of Proxy's
+ * @typedef {Proxy} Request
+ * @example
+ * const api = Request('https://api.jt3ch.net')
+ * 
+ * api
+ *  .this['is']
+ *  .a('test')
+ *  .hi
+ *  .post({
+ *    body: { hi: true },
+ *    query: { test: 'a' }
+ *  })
+ * 
+ * // => POST https://api.jt3ch.net/this/is/a/test/hi?test=a BODY '{"hi": true }'
+ */
