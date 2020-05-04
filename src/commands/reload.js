24f501dc67a1bbaac62b018765e17c757dc340e6
jpbberry <jb@jt3ch.net>
2020-05-04 09:59:09 -0700

    Reload dashboard command

--- src/commands/reload.js
+++ src/commands/reload.js
@@ -1,5 +1,6 @@
 exports.run = async function (message, args) {
-  if (args[0] === 'cluster') this.client.cluster.internal.reloadInternals()
+  if (args[0] === 'cluster') return this.client.cluster.internal.reloadInternals()
+  if (args[0] === 'dash') return this.client.cluster.internal.restartDashboard()
 
   this.client.cluster.internal.reload(args[0])
 
