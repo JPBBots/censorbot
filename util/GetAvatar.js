3df2a0f102cd753433cd237eb91f6c1fd73fd06d
jpbberry <jb@jt3ch.net>
2020-05-04 12:56:05 -0700

    CDN isnt discord.com yet:tm:

--- util/GetAvatar.js
+++ util/GetAvatar.js
@@ -2,7 +2,7 @@ const fetch = require('node-fetch')
 
 module.exports = (id, avatar, discrim = 1) => {
   return new Promise(resolve => {
-    fetch(`https://cdn.discord.com/${!avatar ? 'embed/' : ''}avatars/${!avatar ? discrim % 5 : `${id}/${avatar}`}.png`)
+    fetch(`https://cdn.discordapp.com/${!avatar ? 'embed/' : ''}avatars/${!avatar ? discrim % 5 : `${id}/${avatar}`}.png`)
       .then(x => x.buffer())
       .then(x => resolve(`data:image/png;base64,${x.toString('base64')}`))
   })
