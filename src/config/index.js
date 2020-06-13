5bee54fbb29b08bcaedbdc24491a29cfae6a3ac6
jpbberry <jb@jt3ch.net>
2020-06-13 12:04:52 -0700

    Fix config not loading if missing prod config

--- src/config/index.js
+++ src/config/index.js
@@ -1,10 +1,8 @@
-delete require.cache[require.resolve('./prod/public.js')]
-delete require.cache[require.resolve('./prod/private.js')]
-delete require.cache[require.resolve('./beta/public.js')]
-delete require.cache[require.resolve('./beta/private.js')]
-delete require.cache[require.resolve('./shared.js')]
-
 const beta = process.argv.includes('-b')
+
+delete require.cache[require.resolve(`./${beta ? 'beta' : 'prod'}/public.js`)]
+delete require.cache[require.resolve(`./${beta ? 'beta' : 'prod'}/private.js`)]
+delete require.cache[require.resolve('./shared.js')]
 
 module.exports = {
   ...require(`./${beta ? 'beta' : 'prod'}/public.js`),
