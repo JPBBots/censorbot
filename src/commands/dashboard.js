e87d1988528edbb201cfdadf9115a0c76d104468
jpbberry <jb@jt3ch.net>
2020-06-13 17:57:20 -0700

    Add settings and config as dashboard alias

--- src/commands/dashboard.js
+++ src/commands/dashboard.js
@@ -5,5 +5,5 @@ exports.info = {
   name: 'dashboard',
   description: 'Gives link to the online settings dashboard',
   format: '{prefix}dashboard',
-  aliases: ['dash']
+  aliases: ['dash', 'settings', 'config']
 }
