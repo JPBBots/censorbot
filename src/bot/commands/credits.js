01b74940c11a6a329c934dd11b4b6d3df345a163
Limit11 <66059745+Limit11@users.noreply.github.com>
2021-01-28 19:19:29 -0800

    Updated user tags

--- src/bot/commands/credits.js
+++ src/bot/commands/credits.js
@@ -1,3 +1,4 @@
+
 exports.run = function (message, args) {
   this.send(
     this.embed
@@ -10,7 +11,7 @@ exports.run = function (message, args) {
         '**Moral Support**: [Peter2469](https://www.peternokes.co.uk)\n'
       )
       .field('__**Support Team**__',
-        'Baby Spinel#1866, Egyptian_Lobster#9080, Nevyl#1286'
+        '<@467593308839870475>, <@601784957664493608>, <507444005030854656>'
       )
       .field('__**Translators**__',
         '**English**: Literally everyone\n' +
