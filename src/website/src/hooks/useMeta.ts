c23cd4dd80a8c6d3f60e47a943090e2067bf027e
Julian Berryessa <jb@bit.kiwi>
2022-05-22 16:55:50 -0400

    fix: type issue

--- src/website/src/hooks/useMeta.ts
+++ src/website/src/hooks/useMeta.ts
@@ -2,12 +2,15 @@ import { ServerSideProps } from '@/../../typings'
 import { useEffect, useState } from 'react'
 
 export const useMeta = () => {
-  const [props, setProps] = useState<ServerSideProps>({ serverCount: 1 })
+  const [props, setProps] = useState<ServerSideProps>({
+    serverCount: 1,
+    trialLength: 1
+  })
 
   useEffect(() => {
     fetch('/api/props')
-      .then(x => x.json())
-      .then(props => setProps(props))
+      .then((x) => x.json())
+      .then((props) => setProps(props))
   }, [])
 
   return props
