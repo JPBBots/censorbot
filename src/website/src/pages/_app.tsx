81534227088efc5c37634ed75f13c1309b7f1c56
Julian Berryessa <jb@bit.kiwi>
2022-05-22 17:50:37 -0400

    fix: next zeit babel config

--- src/website/src/pages/_app.tsx
+++ src/website/src/pages/_app.tsx
@@ -27,26 +27,6 @@ export const chargebee =
       (Chargebee?.init({ site: 'censorbot' }) as ChargebeeWeb) ?? null
     : null
 
-/**
- * <meta
-        name="viewport"
-        content="width=device-width, height=device-height, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0"
-      />
-      <title>Censor Bot</title>
-
-      <meta property="og:image" content={BRANDING.logo} />
-      <meta name="theme-color" content="#ea5455" />
-      <meta property="og:title" content={BRANDING.name} />
-      <meta
-        property="og:description"
-        content="Easy to use advanced content filtering for your Discord server(s)!"
-      />
-      <meta
-        name="description"
-        content="Advanced Anti-Swear Bot that comes with a pre-built filters, no work needed! anti swear bot"
-      />
- */
-
 export default function App(props: AppProps) {
   const { Component } = props
 
